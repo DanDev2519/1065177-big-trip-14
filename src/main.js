ebc10621c0425ce9c2ba02399c7ad15d8ebb53ad
DanDev2519 <ostryakov.danil@yandex.ru>
2021-04-14 23:20:33 +0300

    Refactoring render method

--- src/main.js
+++ src/main.js
@@ -11,6 +11,7 @@ import {generatePoint} from './mock/point';
 import {generateDestination} from './mock/destination';
 import {generateOfferList} from './mock/offer';
 import {generateFilter} from './mock/filter';
+import {renderTemplate} from './utils.js';
 
 const TRIP_POINT_COUNT = 4;
 
@@ -19,10 +20,6 @@ const offers = generateOfferList();
 const points = new Array(TRIP_POINT_COUNT).fill().map(() => {return generatePoint(offers);});
 const filters = generateFilter(points);
 
-const render = (container, template, place = 'beforeend') => {
-  container.insertAdjacentHTML(place, template);
-};
-
 const pageHeader = document.querySelector('.page-header');
 const tripMain = pageHeader.querySelector('.trip-main');
 const tripControlsNavigation = tripMain.querySelector('.trip-controls__navigation');
@@ -30,25 +27,25 @@ const tripControlsFilters = tripMain.querySelector('.trip-controls__filters');
 const pageMain = document.querySelector('.page-main');
 const tripEvents = pageMain.querySelector('.trip-events');
 
-render(tripControlsNavigation, createSiteMenuMarkup());
-render(tripMain, createTripInfoMarkup(points), 'afterbegin');
-render(tripControlsFilters, createSiteFiltersMarkup(filters));
+renderTemplate(tripControlsNavigation, createSiteMenuMarkup());
+renderTemplate(tripMain, createTripInfoMarkup(points), 'afterbegin');
+renderTemplate(tripControlsFilters, createSiteFiltersMarkup(filters));
 
 if (points.length) {
-  render(tripEvents, createTripSortMarkup());
-  render(tripEvents, createTripPointListMarkup());
+  renderTemplate(tripEvents, createTripSortMarkup());
+  renderTemplate(tripEvents, createTripPointListMarkup());
 
   const tripEventsList = tripEvents.querySelector('.trip-events__list');
 
-  // render(tripEventsList, createTripAddMarkup(offers, destinations));
+  // renderTemplate(tripEventsList, createTripAddMarkup(offers, destinations));
   const destination = destinations.find((obj) => obj.name === points[0].destination);
   const offerArr = offers.find((obj) => obj.type === points[0].type).offers;
-  render(tripEventsList, createTripEditMarkup(points[0], offerArr, destination));
+  renderTemplate(tripEventsList, createTripEditMarkup(points[0], offerArr, destination));
 
   for (let i = 1; i < TRIP_POINT_COUNT; i++) {
-    render(tripEventsList, createTripPointMarkup(points[i]));
+    renderTemplate(tripEventsList, createTripPointMarkup(points[i]));
   }
 } else {
-  render(tripEvents, createTripMessageMarkup());
+  renderTemplate(tripEvents, createTripMessageMarkup());
 }
 
