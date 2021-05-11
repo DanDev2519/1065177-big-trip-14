aba5579569ea140b17abc0a670ed911f4be47dc9
DanDev2519 <ostryakov.danil@yandex.ru>
2021-05-11 21:58:01 +0300

    fix label for offers

--- src/view/trip-edit.js
+++ src/view/trip-edit.js
@@ -29,9 +29,9 @@ const createSectionOffersMarkup = (offerArr, optionsArr) => {
       <h3 class="event__section-title  event__section-title--offers">Offers</h3>
 
       <div class="event__available-offers">
-        ${offerArr.map(({name = '', cost}) => `<div class="event__offer-selector">
-            <input class="event__offer-checkbox  visually-hidden" id="event-offer-${name.toLowerCase()}-1" type="checkbox" name="event-offer-luggage"  ${optionsArr.filter((opt) => opt.name == name)[0] ? 'checked' : ''}>
-            <label class="event__offer-label" for="event-offer-luggage-1">
+        ${offerArr.map(({name = '', cost}, i) => `<div class="event__offer-selector">
+            <input class="event__offer-checkbox  visually-hidden" id="event-offer-${name.toLowerCase()}-${i}" type="checkbox" name="event-offer-luggage"  ${optionsArr.filter((opt) => opt.name == name)[0] ? 'checked' : ''}>
+            <label class="event__offer-label" for="event-offer-${name.toLowerCase()}-${i}">
               <span class="event__offer-title">${name}</span>
               &plus;&euro;&nbsp;
               <span class="event__offer-price">${cost}</span>
